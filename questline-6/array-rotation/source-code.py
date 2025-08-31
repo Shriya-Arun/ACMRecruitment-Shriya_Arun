723b02fd7a56ee84c3817078272eb48e518b2e73
Shriya-Arun <shriya.arun07@outlook.com>
2025-08-31 13:33:01 +0530

    Update source-code.py

--- questline-6/array-rotation/source-code.py
+++ questline-6/array-rotation/source-code.py
@@ -1,9 +1,11 @@
-n=int(input("enter the number of elements:"))
+#CODE:
+n=int(input("Enter the number of elements:"))
 nums=list(map(int,input("Enter the elements:").split()))
+print("The list before rotation:",nums)
 k=int(input("Enter k:"))
 newlst=nums.copy()
 for i in range(k):
     for j in range(len(nums)):
         newlst[j]=nums[j-1]
     nums=newlst.copy()
-print(nums)
+print("The list after rotation:",nums)
